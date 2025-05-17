eafcc1c18915f2af335ca516fc6edece0cf3283a
Arnav Prasad <thearnavprasad@gmail.com>
2025-05-17 15:31:12 +0530

    added index.ts wasnt working

---
+++ wander-swipe/src/types/index.ts
@@ -0,0 +1,12 @@
+
+export interface Destination {
+    id: string;
+    name: string;
+    country: string;
+    image: string;
+    tags: string[];
+    description: string;
+  }
+  
+  export type ThemeMode = 'light' | 'dark';
+  
